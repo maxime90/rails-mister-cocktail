b2d320c1ecc5fbc515ae98ae9ee26a792cd382fb
Maxime Jeantet <maximejeantet12@gmail.com>
2018-11-23 12:50:26 +0100

    Mr Cocktail Back

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,45 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params_cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+def edit
+  @cocktail = Cocktail.find(params[:id])
+end
+
+def update
+  @cocktail = Cocktail.find(params[:id])
+  @cocktail.update(params_cocktail)
+  redirect_to cocktail_path(@cocktail)
+end
+
+def destroy
+  @cocktail = Cocktail.find(params[:id])
+  @cocktail.destroy
+  redirect_to cocktails_path
+end
+
+private
+
+def params_cocktail
+  params.require(:cocktail).permit(:name)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-
+  resources :cocktails do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
